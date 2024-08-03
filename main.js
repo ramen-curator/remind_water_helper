81b09d921837a419467f7ceac809ba3b26179024
Curator <ramen.curator@gmail.com>
2024-08-03 16:29:37 +0800

    fix: 点击停止的时候漏掉隐藏显示了

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const stopBtn = document.getElementById("stop");
 stopBtn.addEventListener("click", () => {
   reset();
   hide("display");
+  hide("displayForWait");
   hide("stop");
   show("start");
 });
